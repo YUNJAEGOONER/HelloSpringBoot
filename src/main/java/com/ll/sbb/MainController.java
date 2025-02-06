d750150ad77e3c47bde93d341748ac89beef63f2
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2025-02-06 21:00:56 +0900

    create new project

---
+++ src/main/java/com/ll/sbb/MainController.java
@@ -0,0 +1,21 @@
+package com.ll.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/sbb")
+    @ResponseBody //문자열을 반환...
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public String start_page(){
+        return "Hello SpringBoot";
+    }
+}
