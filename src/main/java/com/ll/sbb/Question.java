280acc485f844973cad1911bc1cea519ffb4c754
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2025-02-06 21:37:25 +0900

    Create Entity

---
+++ src/main/java/com/ll/sbb/Question.java
@@ -0,0 +1,29 @@
+package com.ll.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity //Entity : 데이터베이스 테이블과 매핑괴는 자바 클래스
+public class Question {
+
+    @Id //기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200) //열의 세부 설정
+    private String subject;
+
+    @Column(columnDefinition = "Text")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answers;
+}
